8f0561d339f7ac94e91ca02fca5d2a7bdbc78da9
root <root@ip-172-31-29-168.us-east-2.compute.internal>
2019-05-28 12:39:55 +0000

    modified npm install

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -9,10 +9,9 @@ ssh -tt ec2-user@ec2-18-220-71-177.us-east-2.compute.amazonaws.com <<EOF
 cd /home/ec2-user/node-app
 git clone -b Test https://github.com/sharvii/simple-node-js-react-npm-app-master.git 
 cd simple-node-js-react-npm-app-master 
-npm install --production
-set -x
+npm install 
+
 npm run build
-set +x
 
 
 
@@ -22,12 +21,13 @@ set +x
 
 
 
-set -x
+
+
 npm start &
 sleep 1
 echo $! > .pidfile
-set +x
-exit
+
+
 EOF
 
 
