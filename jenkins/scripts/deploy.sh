730d82f6536628396302cc9378ccb9cfdf5869ff
root <root@ip-172-31-29-168.us-east-2.compute.internal>
2019-05-28 11:22:00 +0000

    EOF added

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -5,7 +5,7 @@
 
 
 
-ssh -tt ec2-user@ec2-18-220-71-177.us-east-2.compute.amazonaws.com
+ssh -tt ec2-user@ec2-18-220-71-177.us-east-2.compute.amazonaws.com <<EOF
 cd /home/ec2-user/node-app
 git clone -b Test https://github.com/sharvii/simple-node-js-react-npm-app-master.git 
 npm install --production
@@ -26,7 +26,8 @@ npm start &
 sleep 1
 echo $! > .pidfile
 set +x
-
+exit
+EOF
 
 
 
