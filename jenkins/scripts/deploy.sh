b4013da8198dfdf71818e5367ab80adf2253b1bb
root <root@ip-172-31-29-168.us-east-2.compute.internal>
2019-05-28 10:50:56 +0000

    modified jenkinsfile

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -7,7 +7,7 @@
 
 ssh -tt ec2-user@ec2-18-220-71-177.us-east-2.compute.amazonaws.com
 cd /home/ec2-user/node-app
-git pull origin Test
+git clone -b Test https://github.com/sharvii/simple-node-js-react-npm-app-master.git 
 npm install --production
 set -x
 npm run build
