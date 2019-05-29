ff2066d31fc226090f15658bfbea24faa97908d0
root <root@ip-172-31-29-168.us-east-2.compute.internal>
2019-05-29 07:47:47 +0000

    modified jenkinsfile

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env sh
 
-
-
-
-
-
 ssh -tt ec2-user@ec2-18-220-71-177.us-east-2.compute.amazonaws.com <<EOF
 cd /home/ec2-user/node-app
-git clone -b Test https://github.com/sharvii/simple-node-js-react-npm-app-master.git 
-cd simple-node-js-react-npm-app-master 
-npm install 
+git clone -b Test https://github.com/sharvii/simple-node-js-react-npm-app-master.git
+cd simple-node-js-react-npm-app-master
+npm install
 
+
+
+
+
+set -x
 npm run build
+set +x
 
 
 
@@ -21,15 +22,13 @@ npm run build
 
 
 
-
-
+set -x
 npm start &
 sleep 1
 echo $! > .pidfile
-
-
-EOF
+set +x
 
 
 
 
+echo 'created your initial Pipeline as a Jenkinsfile.)'
