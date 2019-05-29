59dadb0fa1f015b29fd4b28dc2044a36897c5e54
root <root@ip-172-31-29-168.us-east-2.compute.internal>
2019-05-29 08:05:47 +0000

    modified files

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-echo 'The following command terminates the "npm start" process using its PID'
-echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
-echo 'was executed.'
+
+
+echo 'kill the process'
 set -x
 kill $(cat .pidfile)
